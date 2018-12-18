fb77e49ab3c135724ebd8f2da798f9f7e5fa941a
AustinP <rockmanvnx6@gmail.com>
2018-12-18 16:15:34 +1100

    modified database

--- models/employeeSchedule.js
+++ models/employeeSchedule.js
@@ -15,7 +15,23 @@ var EmployeeScheduleSchema = new Schema({
     type: String,
     default: ""
   },
+  monday_title: {
+    type: String,
+    default: ""
+  },
+  monday_des: {
+    type: String,
+    default: ""
+  },
   tuesday: {
+    type: String,
+    default: ""
+  },
+  tuesday_title: {
+    type: String,
+    default: ""
+  },
+  tuesday_des: {
     type: String,
     default: ""
   },
@@ -23,7 +39,23 @@ var EmployeeScheduleSchema = new Schema({
     type: String,
     default: ""
   },
+  wednesday_title: {
+    type: String,
+    default: ""
+  },
+  wednesday_des: {
+    type: String,
+    default: ""
+  },
   thursday: {
+    type: String,
+    default: ""
+  },
+  thursday_title: {
+    type: String,
+    default: ""
+  },
+  thursday_des: {
     type: String,
     default: ""
   },
@@ -31,11 +63,35 @@ var EmployeeScheduleSchema = new Schema({
     type: String,
     default: ""
   },
+  friday_title: {
+    type: String,
+    default: ""
+  },
+  friday_des: {
+    type: String,
+    default: ""
+  },
   saturday:{
     type: String,
     default: ""
   },
+  saturday_title: {
+    type: String,
+    default: ""
+  },
+  saturday_des: {
+    type: String,
+    default: ""
+  },
   sunday: {
+    type: String,
+    default: ""
+  },
+  sunday_title: {
+    type: String,
+    default: ""
+  },
+  sunday_des: {
     type: String,
     default: ""
   },
@@ -46,6 +102,34 @@ var EmployeeScheduleSchema = new Schema({
   active: {
     type: Number,
     default: 1,
+  },
+  monday_accept: {
+    type: boolean,
+    default: false
+  },
+  tuesday_accept: {
+    type: boolean,
+    default: false
+  },
+  wednesday_accept: {
+    type: boolean,
+    default: false
+  },
+  thursday_accept: {
+    type: boolean,
+    default: false
+  },
+  friday_accept: {
+    type: boolean,
+    default: false
+  },
+  saturday_accept: {
+    type: boolean,
+    default: false
+  },
+  sunday_accept: {
+    type: boolean,
+    default: false
   }
 });
 
