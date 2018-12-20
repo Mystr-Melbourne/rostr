392dec1f1467e618db3aef28680cd92777e8a611
MichaelDao <daoduymichael@gmail.com>
2018-12-20 20:12:59 +1100

    removed announcement

--- app/components/children/ManagerHome.js
+++ app/components/children/ManagerHome.js
@@ -1,59 +1,56 @@
 var React = require("react");
 var helpers = require("../utils/helpers");
-var ScheduleView =  require("./ScheduleView");
-var AnnouncementsBuild =  require("./AnnouncementsBuild");
-var AnnouncementsView =  require("./AnnouncementsView");
+var ScheduleView = require("./ScheduleView");
+var AnnouncementsBuild = require("./AnnouncementsBuild");
+var AnnouncementsView = require("./AnnouncementsView");
 
 var ManagerHome = React.createClass({
-    getInitialState: function() {
-        return {
-            title: "",
-            content: "",
-            department: "",
-        };
-    },
+  getInitialState: function() {
+    return {
+      title: "",
+      content: "",
+      department: ""
+    };
+  },
 
-    componentDidMount: function() {
-        this.getAnnouncements();
-        this.getAllDepartments();
-    },
+  componentDidMount: function() {
+    this.getAnnouncements();
+    this.getAllDepartments();
+  },
 
-    // componentDidUpdate: function(prevState) {
-    //     if (prevState.title !== this.state.title || prevState.content !== this.state.content) {
-    //         this.getAnnouncements();
-    //     }
-    // },
-    getAllDepartments: function() {
-        helpers.getAllDepartments().then(function(response){
-            this.setState({
-                department: response.data.department
-            });
-        }.bind(this));
-    },
-    getAnnouncements: function() {
-        helpers.getAnnouncements().then(function(response) {
-          this.setState({
-            title: response.data[response.data.length -1].title,
-            content: response.data[response.data.length -1].content
-          });
-        }.bind(this));
-    },
+  // componentDidUpdate: function(prevState) {
+  //     if (prevState.title !== this.state.title || prevState.content !== this.state.content) {
+  //         this.getAnnouncements();
+  //     }
+  // },
+  getAllDepartments: function() {
+    helpers.getAllDepartments().then(
+      function(response) {
+        this.setState({
+          department: response.data.department
+        });
+      }.bind(this)
+    );
+  },
+  getAnnouncements: function() {
+    helpers.getAnnouncements().then(
+      function(response) {
+        this.setState({
+          title: response.data[response.data.length - 1].title,
+          content: response.data[response.data.length - 1].content
+        });
+      }.bind(this)
+    );
+  },
 
-    render: function() {
-        return (
-            <div>
-                <ScheduleView />
-                <div className="row">
-                    <div className="col m6">
-                        <AnnouncementsView title={this.state.title} content={this.state.content}/>
-                    </div>
-                    <div className="col m6">
-                        <AnnouncementsBuild />
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render: function() {
+    return (
+      <div>
+        <ScheduleView />
+        <AnnouncementsBuild />
+      </div>
+    );
+  }
 });
 
 module.exports = ManagerHome;
