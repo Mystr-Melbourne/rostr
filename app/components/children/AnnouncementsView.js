b4faefc4b88328d98d6294e5b14f78e7b96909f6
Inci <inci90@users.noreply.github.com>
2018-12-29 21:21:27 +1100

    changing this.props.title to this.props.location

--- app/components/children/AnnouncementsView.js
+++ app/components/children/AnnouncementsView.js
@@ -6,13 +6,13 @@ var AnnouncementsView = React.createClass({
       <div className="card-panel">
         <div className="row">
           <div className="col s12">
-            <h5>Latest announcement</h5>
+            <h5>Latest Announcement</h5>
           </div>
         </div>
         <div className="row">
           <div className="col s6">
-            {/* <h5>{this.state.title}</h5> */}
-            <h5>{this.props.title}</h5>
+            {/* <h5>{this.state.location}</h5> */}
+            <h5>{this.props.location}</h5>
             <p>{this.props.content}</p>
             {/* <p>{this.state.content}</p> */}
           </div>
