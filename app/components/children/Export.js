f6eb1d3d4b9b758c707b188448da897fea472b59
Inci <inci90@users.noreply.github.com>
2018-12-31 14:28:03 +1100

    creating front-end for CSV export

---
+++ app/components/children/Export.js
@@ -0,0 +1,48 @@
+var React = require("react");
+var helpers = require("../utils/helpers");
+
+var Export = React.createClass({
+
+    getInitialState: function () {
+        return {
+            username: "",
+            picture: ""
+        };
+    },
+
+    componentDidMount: function () {
+        helpers.getCurrentUser().then(function (response) {
+            if (response !== this.state.username) {
+                this.setState({ picture: response.data.picture, username: response.data.username });
+            }
+        }.bind(this));
+    },
+
+    render: function () {
+        return (
+            <div>
+                <br></br>
+                <div className="row">
+                    <div className="col s4">
+                        <button id="Export Excel" className="btn btn-large waves-effect waves-light green accent-3" type="submit" value="Submit" form="addNew">Export Excel File
+                                        <i className="material-icons right">insert_drive_file</i>
+                        </button>
+                    </div>
+                    <div className="col s4">
+                        <a id="Export Google Sheets" className="btn btn-large waves-effect waves-light blue accent-3" onClick={this.handleUpdateForm}>Export Google Sheets
+                                        <i className="material-icons right">insert_drive_file</i>
+                        </a>
+                    </div>
+                    <div className="col s4">
+                        <a id="Export CSV Text File" className="btn btn-large waves-effect waves-light red accent-3" onClick={this.handleRemoveForm}>Export CSV Text File
+                                        <i className="material-icons right">insert_drive_file</i>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = Export;
