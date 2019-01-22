aa6b26f5b4cd3b4423a8a5be73e061703bfd3ac1
MichaelDao <daoduymichael@gmail.com>
2019-01-22 19:01:37 +1100

    Revert "Revert "Merge pull request #69 from Mystr-Melbourne/buttonArrange""
    This reverts commit b91204a7f60ff34c8881efd26b4eb5c8387dd121.

--- app/config/routes.js
+++ app/config/routes.js
@@ -15,7 +15,6 @@ var Manager = require("../components/Manager");
 var ManagerHome = require("../components/children/ManagerHome");
 var ManagerEmployeeAll = require("../components/children/ManagerEmployeeAll");
 var ManagerSchedulesCreate = require("../components/children/ManagerSchedulesCreate");
-var Export = require("../components/children/Export");
 var AnnouncementsBuild = require("../components/children/AnnouncementsBuild");
 // employee components
 var Employee = require("../components/Employee");
@@ -30,7 +29,6 @@ module.exports = (
             <Route path="manager" component={Manager}>
                 <Route path="employeeAll" component={ManagerEmployeeAll} />
                 <Route path="schedulesCreate" component={ManagerSchedulesCreate} />
-                <Route path="export" component={Export} />
                 <Route path="assignShift" component={AnnouncementsBuild} />
                 <IndexRoute component={ManagerHome} />
             </Route>
